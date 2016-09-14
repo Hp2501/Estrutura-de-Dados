1109f92049947f106a7addd2cc0d760da67e1a19
Marcelo Lino <mdslino@gmail.com>
2016-09-14 17:55:43 -0300

    alterações
    alterações

--- TP02/Multiplicação Matriz/MultiMatriz.c
+++ TP02/Multiplicação Matriz/MultiMatriz.c
@@ -1,34 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
-void criarMatriz(){
-    int *mat1, *mat2;
-    int c1, l1, c2, l2;
-    printf("Digite o tamanho da coluna da primeira matriz\n");scanf("%d", &c1);
-    printf("Digite o tamanho da linha da primeira matriz\n");scanf("%d", &l1);
-    printf("Digite o tamanho da coluna da segunda matriz\n");scanf("%d", &c2);
-    printf("Digite o tamanho da linha da segunda matriz\n");scanf("%d", &l2);
-    if(l1==c2)
+void iniciarMatriz(mat1, mat2 int l1, int l2, int c1, int c2, int diml1, int diml2, int dimc1, int dimc2)
+{
+    for(l1=0;l1<diml1;l1++)
+        for(c1=0;c1<dimc1;c1++)
     {
-        mat1 = malloc(l1 * c1 * sizeof(int));
-        mat2 = malloc(l2 * c2 * sizeof(int));
+        printf("Elemento[%d][%d]", l1+1, c1+1);
+        scanf("%d", *mat1[l1][c1]);
     }
-    else
-    {
-        printf("ERRO!! DIMENSOES IMCOMPATIVEIS\n\n ");
-        criarMatriz();
 
+    for(l2=0;l2<diml2;l2++)
+        for(c2=0;c2<dimc2;c2++)
+    {
+        printf("Elemento[%d][%d]", l2+1, c2+1);
+        scanf("%d", *mat1[l2][c2]);
     }
 }
 
-int main(){
-    int i, j;
-    criarMatriz();
-    for (i=0; i<3; i++)
-        for (j=0; j<3; j++)
+void checarMatriz(int diml2, int dimc1)
+{
+    if(dimc1!=diml2)
     {
-        printf("%d", mat1[i][j]);
-        printf("%d", mat1[i][j]);
+        printf("Matriz imcompativel");
     }
+    else
+    {
+        iniciarMatriz(mat1, mat2 int l1, int l2, int c1, int c2, int diml1, int diml2, int dimc1, int dimc2)
+    }
+}
 
+int produtoMatriz(mat1, mat2 int l1, int l2, int c1, int c2, int diml1, int diml2, int dimc1, int dimc2)
+{
+    mf = malloc(sizeof(int) * dimc1 * diml2)
 
 }
