88eda1f7627eb7aaa879ba3b769ea87b6a036cc5
Marcelo Lino <mdslino@gmail.com>
2016-10-18 13:22:53 -0200

    Elevador

---
+++ TP03/elevador.py
@@ -0,0 +1,30 @@
+fila = [[2, 1], [4, 3], [5, 0]]
+
+def andarChamando():
+    i = 0
+    controle = []
+    while i < len(fila):
+        andar = fila[i]
+        controle.append(andar[0])
+        i += 1
+    return controle
+
+def andarParaIr():
+    i = 0
+    controle = []
+    while i < len(fila):
+        andar = fila[i]
+        controle.append(andar[1])
+        i += 1
+        controle.
+    return controle
+
+def movimentoChamando():
+    for i in andarChamando().pop(0):
+        print("Teste")
+
+
+
+print(movimentoChamando())
+#print(movimento())
+
